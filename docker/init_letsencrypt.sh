95a33c9375cb404a09bf8acae6c09f5efb84cdf4
georg220022 <104333621+georg220022@users.noreply.github.com>
2022-10-23 19:18:40 +0300

    Update init_letsencrypt.sh

--- docker/init_letsencrypt.sh
+++ docker/init_letsencrypt.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
@@ -8,8 +6,8 @@ fi
 domains=(e-lnk.ru eeozon.ru eewildberries.ru eealiexpress.ru)
 rsa_key_size=4096
 data_path="./certbot"
-email="XXXXXXXXXXXXXX" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="XXXXXXXXXXXXXX"
+staging=1
 
 if [ -d "$data_path" ]; then
   read -p "Уже есть сертификаты для $domains. Продолжить и заменить их (y/N) " decision
@@ -51,19 +49,17 @@ echo
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# Select appropriate email arg
+
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
   *) email_arg="--email $email" ;;
 esac
 
-# Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
